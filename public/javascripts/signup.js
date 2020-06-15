316784db10cb1b6c3573d880b8c3c04598d2215d
jjaisee <42540983+jjaisee@users.noreply.github.com>
2020-06-15 19:51:49 +0530

    popup above all

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -1,6 +1,14 @@
-document.getElementsByClassName("aaaaa")[0].addEventListener("click", function(){
+const signupButtons = document.getElementsByClassName("button-signup-popup");
+let signupModal = function(){
   document.querySelector(".signup-popup").style.display = "flex";
-});
+}
+for (let i = 0; i < signupButtons.length; i++) {
+    signupButtons[i].addEventListener("click", signupModal, false);
+}
+
+// document.getElementsByClassName("button-signup-popup")[0].addEventListener("click", function(){
+//   document.querySelector(".signup-popup").style.display = "flex";
+// });
 document.querySelector(".close").addEventListener("click",function(){
   document.querySelector(".signup-popup").style.display = "none";
 });
