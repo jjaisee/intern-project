f27dc822ae7b638b8c6b44c9d7e4ea03f86e3c72
jjaisee <42540983+jjaisee@users.noreply.github.com>
2020-06-18 17:42:44 +0530

    authentication google and local

--- app.js
+++ app.js
@@ -1,28 +1,107 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const favicon = require("serve-favicon");
 var path = require('path')
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
-const app = express();
+
 
 const categoriesImg = ["outdoorAndAdventure", "music", "writing", "friendsAndFamily", "languageAndCulture", "film", "learning", "social", "healthAndWellness", "maritalConsulting","dance", "foodAndDrink","photography","beliefs","sci-fiAndGames","artAndCraft","bookClubs","pets","fashionAndBeauty","career","sportsAndFitness","technology"];
 const categoriesTitle = ["Letz Hike", "Letz Create Music", "Letz Writing", "Friends & Family", "Language & Culture", "Letz Film", "Letz Learning", "Letz Connect", "Health & Wellness", "Letz Find Maritial Counselors", "Letz Dance", "Letz Cook","Letz Click","Beliefs","Sci-Fi & Games","Letz Draw","Book Clubs","Pets","Letz Fashion","Career","Sports & Fitness","Technology"];
 
 
-app.set("view engine", "ejs");
+const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.use(favicon(path.join(__dirname, 'public', 'images/favicon.ico')));
 
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get("/",function(req,res){
-  res.render("home",{
-    categoriesImg : categoriesImg,
-    categoriesTitle: categoriesTitle
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+  googleId: String,
+  name: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+
+const User = new mongoose.model("User",userSchema);
+
+ passport.use(User.createStrategy());
+
+
+
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
   });
 });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // console.log(profile);
+    User.findOrCreate({ googleId: profile.id, name: profile.displayName }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get("/",function(req,res){
+  if(req.isAuthenticated()){
+    res.render("user-home");
+  } else{
+
+    res.render("home",{
+      categoriesImg : categoriesImg,
+      categoriesTitle: categoriesTitle
+    });
+
+  }
+});
+
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get('/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  function(req, res) {
+    // Successful authentication, redirect user home.
+    res.redirect('/user-home');
+  });
+
+
 app.get("/events",function(req,res){
   res.render("events");
 });
@@ -32,8 +111,54 @@ app.get("/contactus",function(req,res){
 app.get("/about",function(req,res){
   res.render("about");
 });
+app.get("/user-home",function(req,res){
+  if(req.isAuthenticated()){
+    res.render("user-home");
+  } else{
+    res.redirect("/");
+  }
+});
+app.get("/signup",function(req,res){
+  res.render("signup");
+});
 
+app.get("/logout",function(req,res){
+  req.logout();
+  res.redirect("/");
+});
 
+app.post("/signup",function(req,res){
+
+  User.register({username:req.body.username}, req.body.password, function(err,user){
+    if(err){
+      console.log(err);
+      res.redirect("/signup");
+    } else{
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/user-home");
+      });
+    }
+  });
+});
+
+app.post("/header",function(req,res){
+
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
+  });
+  req.login(user, function(err){
+    if(err){
+      res.redirect("/");
+      console.log(err);
+    } else{
+      passport.authenticate("local",{failureFlash: "Invalid username or password." })(req,res,function(){
+        res.redirect("/user-home");
+      });
+    }
+  });
+
+});
 
 
 app.listen(3000,function(){
--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -1,14 +1,38 @@
+document.getElementById("signup-with-email").onclick = function () {
+        location.href = "/signup";
+    };
+
+
 const signupButtons = document.getElementsByClassName("button-signup-popup");
 let signupModal = function(){
   document.querySelector(".signup-popup").style.display = "flex";
+  document.querySelector(".login-popup").style.display = "none";
 }
 for (let i = 0; i < signupButtons.length; i++) {
     signupButtons[i].addEventListener("click", signupModal, false);
 }
 
-// document.getElementsByClassName("button-signup-popup")[0].addEventListener("click", function(){
-//   document.querySelector(".signup-popup").style.display = "flex";
-// });
-document.querySelector(".close").addEventListener("click",function(){
+const loginButtons = document.getElementsByClassName("button-login-popup");
+let loginModal = function(){
+  document.querySelector(".login-popup").style.display = "flex";
   document.querySelector(".signup-popup").style.display = "none";
-});
+}
+for (let i = 0; i < loginButtons.length; i++) {
+    loginButtons[i].addEventListener("click", loginModal, false);
+}
+
+// document.getElementsByClassName("button-login-popup")[0].addEventListener("click", function(){
+//   document.querySelector(".login-popup").style.display = "flex";
+// });
+
+const closes = document.getElementsByClassName("close");
+let closeModal = function(){
+  document.querySelector(".signup-popup").style.display = "none";
+  document.querySelector(".login-popup").style.display = "none";
+}
+for (let i = 0; i < closes.length; i++) {
+    closes[i].addEventListener("click", closeModal, false);
+}
+// document.querySelector(".close").addEventListener("click",function(){
+//   document.querySelector(".signup-popup").style.display = "none";
+// });
