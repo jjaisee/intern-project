33145538da4b315f1edec46a7e11fced3ff6bbbf
jjaisee <42540983+jjaisee@users.noreply.github.com>
2020-06-18 18:01:31 +0530

    deleted env file and set new api ans secrets

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "http://localhost:3000/auth/google/letzget2gether",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
@@ -94,7 +94,7 @@ app.get("/auth/google",
   passport.authenticate("google", { scope: ["profile"] })
 );
 
-app.get('/auth/google/secrets',
+app.get('/auth/google/letzget2gether',
   passport.authenticate('google', { failureRedirect: '/' }),
   function(req, res) {
     // Successful authentication, redirect user home.
